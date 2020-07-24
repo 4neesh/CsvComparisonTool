6557c6f33cdca259f72afe676691730317cc80bd
Aneesh <aneesh@sky.com>
2020-07-24 06:56:15 +0100

    init

---
+++ VendorOrderMerge/src/main/java/com/aneesh/VendorOrderMergeApplication.java
@@ -0,0 +1,13 @@
+package com.aneesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorOrderMergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorOrderMergeApplication.class, args);
+	}
+
+}
